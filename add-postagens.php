7d25617dce7d5ce6687b6e0277eec1e7471755b6
gabriel <mrgabrielbeirigo28@gmail.com>
2026-05-15 22:30:46 -0300

    Atualização do dashboard

--- add-postagens.php
+++ add-postagens.php
@@ -29,15 +29,20 @@
                     </div>
 
                     <div class="space">
+                        <label for="icat">Categoria</label>
+                        <input type="text" name="cat" id="icat" placeholder="Qual a categoria da sua postagem?">
+                    </div>
+
+                    <div class="space">
                         <label for="istats">Status</label>
                         <select name="stats" id="istats">
-                            <option>Ativo</option>
-                            <option>Inativo</option>
+                            <option>ativo</option>
+                            <option>inativo</option>
                         </select>
                     </div>
         
                     <div class="space">
-                        <input type="submit" value="Salvar" class="esp-btn">
+                        <input type="submit" value="Salvar" class="esp-btn" id="btn-salvar">
                         <input type="reset" value="Limpar" class="esp-btn">
                     </div>
                 </form>
@@ -47,5 +52,30 @@
     <footer>
         <?php include("rodape.php"); ?>
     </footer>
+    <script>
+        let btnSalvar = document.getElementById('btn-salvar')
+        btnSalvar.addEventListener('click', () => {
+            let titulo = document.getElementById('inome').value
+            let conteudo = document.getElementById('icont').value
+            let categoria = document.getElementById('icat').value
+            let status = document.getElementById('istats').value
+
+            if(titulo != '' && conteudo != ''){
+                const newPost = {
+                    postTitle: titulo,
+                    postCont: conteudo,
+                    postCat: categoria,
+                    postStats: status
+                }
+
+                let postagens = JSON.parse(localStorage.getItem('bancoPostagens'))|| []
+                postagens.push(newPost)
+                localStorage.setItem('bancoPostagens', JSON.stringify(postagens))
+                alert("Postagem cadastrada!!")
+            } else{
+                alert("Dados incompletos!")
+            }
+        })
+    </script>
 </body>
 </html>
--- categorias.php
+++ categorias.php
@@ -3,117 +3,79 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/52ff4c741b.js" crossorigin="anonymous"></script>
-    <title>Categorias</title>
+    <title>Adicionar categoria</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="tabelas.css">
-    <style>
-        #tableh h1{
-            margin: 10px;
-        }
-
-        #tableh{
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between; 
-            align-items: center;
-            margin: 10px;
-        }
-
-        #tableh  button{
-            border-radius: 5px;
-            border: 1px solid black;
-            padding: 10px;
-            background-color: #2743c0;
-            color: white;
-            cursor: pointer;
-            transition-duration: .2s;
-        }
-
-        #tableh  button:hover{
-            background-color: #5272ff;
-        }
-
-        #tableh i{
-            margin-right: 5px;
-        }
-    </style>
+    <link rel="stylesheet" href="form.css">
+    <script src="https://kit.fontawesome.com/52ff4c741b.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="sep">
-        <header><?php include("menu.php");?></header>
+        <header>
+            <?php include("menu.php");?>
+        </header>
         <main>
-           <div id="tableh">
-                <h1>Gestão de categorias</h1>
-                <a href="add-categorias.php"><button class="add"><i class="fa-solid fa-user-plus"></i> Adicionar categoria</button></a>
-            </div>
-            <div id="table-container">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Nome</th>
-                            <th>Status</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
-            </div>
+            <section>
+                <h1><i class="fa-solid fa-user-plus"></i> Cadastro de postagem</h1>
+                <p>Preencha os dados para adicionar postagem</p>
+                <form action="#" method="POST">
+                    <div class="space">
+                        <label for="inome">Título</label>
+                        <input type="text" name="nome" id="inome" placeholder="Digite o título da postagem">
+                    </div>
+        
+                    <div class="space">
+                        <label for="icont">Conteúdo</label>
+                        <textarea name="cont" id="icont" placeholder="Sobre o que fala sua postagem??"></textarea>
+                    </div>
+
+                    <div class="space">
+                        <label for="icat">Categoria</label>
+                        <input type="text" name="cat" id="icat" placeholder="Qual a categoria da sua postagem?">
+                    </div>
+
+                    <div class="space">
+                        <label for="istats">Status</label>
+                        <select name="stats" id="istats">
+                            <option>ativo</option>
+                            <option>inativo</option>
+                        </select>
+                    </div>
+        
+                    <div class="space">
+                        <input type="submit" value="Salvar" class="esp-btn" id="btn-salvar">
+                        <input type="reset" value="Limpar" class="esp-btn">
+                    </div>
+                </form>
+            </section>
         </main>
     </div>
-    <footer><?php include("rodape.php");?></footer>
+    <footer>
+        <?php include("rodape.php"); ?>
+    </footer>
     <script>
-        function carregarDados(){
-            let corpoTable = document.querySelector('tbody')
-            let categorias = JSON.parse(localStorage.getItem('bancoCategorias')) || []
-            let id = 0
-            corpoTable.innerHTML = ''
+        let btnSalvar = document.getElementById('btn-salvar')
+        btnSalvar.addEventListener('click', () => {
+            let titulo = document.getElementById('inome').value
+            let conteudo = document.getElementById('icont').value
+            let categoria = document.getElementById('icat').value
+            let status = document.getElementById('istats').value
 
-            categorias.forEach(function(categoria, index){
-                corpoTable.innerHTML += `
-                    </tr>
-                        <td>${id + 1}</td>
-                        <td>${categoria.catName}</td>
-                        <td><mark class="badge ${categoria.catStats}">${categoria.catStats}</mark></td>
-                        <td>
-                            <i class="fa-solid fa-pen" onclick = "editar(${index})"></i>
-                            <i class="fa-solid fa-trash-can" onclick = "deletar(${index})"></i>
-                        </td>
-                    </tr>
-                `
-
-                id += 1
-            })
-        }
-
-        function deletar(index){
-            if(confirm("Tem certeza que deseja deletar essa categoria??")){
-                let categorias = JSON.parse(localStorage.getItem('bancoCategorias'))
-                categorias.splice(index, 1)
-                localStorage.setItem('bancoCategorias', JSON.stringify(categorias))
-                carregarDados()
-            }
-        }
-        
-        function editar(index){
-            let novoNome = prompt("Qual o novo nome da categoria?")
-            let categorias = JSON.parse(localStorage.getItem('bancoCategorias')) 
-            categorias[index].catName = novoNome
-            if(confirm("Deseja mudar o status da categoria?")){
-                if(categorias[index].catStats == 'ativo'){
-                    categorias[index].catStats = 'inativo'
-                } else if(categorias[index].catStats == 'inativo'){
-                    categorias[index].catStats = 'ativo'
+            if(titulo != '' && conteudo != ''){
+                const newPost = {
+                    postTitle: titulo,
+                    postCont: conteudo,
+                    postCat: categoria,
+                    postStats: status
                 }
-            } 
-            localStorage.setItem('bancoCategorias', JSON.stringify(categorias))
-            carregarDados()
-        }
 
-        carregarDados()
-
+                let postagens = JSON.parse(localStorage.getItem('bancoPostagens'))|| []
+                postagens.push(newPost)
+                localStorage.setItem('bancoPostagens', JSON.stringify(postagens))
+                alert("Postagem cadastrada!!")
+            } else{
+                alert("Dados incompletos!")
+            }
+        })
     </script>
 </body>
-</html> 
+</html>
